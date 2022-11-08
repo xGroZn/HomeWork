37f8a2db013def3747807c32b9c56320cf77bd26
xGroZn <av_budnik@mail.ru>
2022-11-09 01:07:27 +0300

    add homework 8 (Strings)

--- src/Calculator/Subtraction.java
+++
@@ -1,9 +0,0 @@
-package Calculator;
-
-public class Subtraction implements Calculator {
-    @Override
-    public double action(double firstValue, double secondValue) {
-        double value = firstValue - secondValue;
-        return value;
-    }
-}
